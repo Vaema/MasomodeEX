427e0b9564d7432a89ba914aa78aba66aaa3d95e
Expert <oofoofdlshog@gmail.com>
2025-06-07 17:48:27 +0300

    Siblings hotfix

--- Content/NPCs/SiblingsSpeedUp.cs
+++ Content/NPCs/SiblingsSpeedUp.cs
@@ -8,14 +8,24 @@ namespace MasomodeEX.Content.NPCs
 {
     public class SiblingsSpeedUp : GlobalNPC
     {
-        public override void PostAI(NPC npc)
+        public override bool InstancePerEntity => true;
+        public int FUCKINGINT;
+
+        public override bool PreAI(NPC npc)
         {
             if (npc.type == ModContent.NPCType<MutantBoss>())
             {
-                if (npc.ai[1] % 12 == 0 && npc.ai[0] != -5) // boost ai except for final spark
+                if (npc.ai[1] % 12 == 0 && npc.ai[0] != -5 && FUCKINGINT < 1) // boost ai except for final spark
+                {
+                    FUCKINGINT++;
                     npc.AI();
-                else if (npc.ai[0] == -5 && npc.ai[1] % 60 == 0) // slightly boost final spark
+                }
+
+                else if (npc.ai[0] == -5 && npc.ai[1] % 60 == 0 && FUCKINGINT < 1) // slightly boost final spark
+                {
+                    FUCKINGINT++;
                     npc.AI();
+                }
 
                 if (npc.ai[0] != 13 || npc.ai[0] != 14 || npc.ai[0] != 15)
                     npc.Center = npc.Center + npc.velocity * 0.05f;
@@ -27,20 +37,35 @@ namespace MasomodeEX.Content.NPCs
 
             if (npc.type == ModContent.NPCType<AbomBoss>())
             {
-                if (npc.ai[1] % 20 == 0)
+                if (npc.ai[1] % 20 == 0 && FUCKINGINT < 1)
+                {
+                    FUCKINGINT++;
                     npc.AI();
+                }
 
                 npc.Center = npc.Center + npc.velocity * 0.05f;
             }
 
             if (npc.type == ModContent.NPCType<DeviBoss>())
             {
-                if (npc.ai[1] % 32 == 0)
+                if (npc.ai[1] % 32 == 0 && FUCKINGINT < 1)
+                {
+                    FUCKINGINT++;
                     npc.AI();
+                }
 
                 npc.Center = npc.Center + npc.velocity * 0.03f;
             }
 
+            return base.PreAI(npc);
+        }
+
+        public override void PostAI(NPC npc)
+        {
+            if (FUCKINGINT == 1)
+            {
+                FUCKINGINT = 0;
+            }
             base.PostAI(npc);
         }
     }
